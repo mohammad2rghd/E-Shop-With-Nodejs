b3489de23f886f4fb368d4ad8565f40bec35117e
mohammad2rghd <mohammad2rmad@gmail.com>
2024-06-28 02:01:32 +0330

    added category model and updated product model

--- models/product.js
+++ models/product.js
@@ -22,7 +22,12 @@ const productSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
-    }
+    },
+    categories: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }]
 });
 
 module.exports = mongoose.model('Product', productSchema);

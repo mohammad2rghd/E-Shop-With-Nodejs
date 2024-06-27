890bb8afc7dec17c7326a69748b174a7edf26d3c
mohammad2rghd <mohammad2rmad@gmail.com>
2024-06-28 01:33:03 +0330

    added user model

---
+++ app.js
@@ -0,0 +1,10 @@
+//built-in packages
+const path = require('path');
+
+//installed packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const flash = require('connect-flash');
